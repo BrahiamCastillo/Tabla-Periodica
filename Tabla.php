cba8de6f569adbbf4840d7a487065ba9084ef3ea
BrahiamCastillo <mymoviehb@gmail.com>
2020-10-19 20:45:51 -0400

    primero

---
+++ Tabla.php
@@ -0,0 +1,52 @@
+<?php 
+  $componentes=array(["elemento"=>"H", "clase"=>"otros-no-metales"],
+  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
+  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
+  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
+  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
+  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"He", "clase"=>"gases-nobles"],
+  ["elemento"=>"Li", "clase"=>"alcalinos"],["elemento"=>"Be", "clase"=>"alcalinoterreos"],
+
+  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
+  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
+  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
+
+  ["elemento"=>"B", "clase"=>"metaloides"],
+  ["elemento"=>"C", "clase"=>"otros-no-metales"],["elemento"=>"N", "clase"=>"otros-no-metales"],["elemento"=>"O", "clase"=>"otros-no-metales"],
+  ["elemento"=>"F", "clase"=>"halogeno"],["elemento"=>"Ne", "clase"=>"gases-nobles"],["elemento"=>"Na", "clase"=>"alcalinos"],
+  ["elemento"=>"Mg", "clase"=>"alcalinoterreos"],
+  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
+  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
+  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
+  
+  ["elemento"=>"Al", "clase"=>"otros-metales"],["elemento"=>"Si", "clase"=>"metaloides"],["elemento"=>"P", "clase"=>"otros-no-metales"],["elemento"=>"S", "clase"=>"otros-no-metales"],
+  ["elemento"=>"Cl", "clase"=>"halogenos"],["elemento"=>"Ar", "clase"=>"gases-nobles"],["elemento"=>"K", "clase"=>"alcalinos"],["elemento"=>"Ca", "clase"=>"alcalinoterreos"],
+  ["elemento"=>"Sc", "clase"=>"metales"],["elemento"=>"Ti", "clase"=>"metales"],["elemento"=>"V", "clase"=>"metales"],["elemento"=>"Cr", "clase"=>"metales"],["elemento"=>"Mn", "clase"=>"metales"],
+  ["elemento"=>"Fe", "clase"=>"metales"],["elemento"=>"Co", "clase"=>"metales"],["elemento"=>"Ni", "clase"=>"metales"],["elemento"=>"Cu", "clase"=>"metales"],["elemento"=>"Zn", "clase"=>"metales"],
+  ["elemento"=>"Ga", "clase"=>"otros-metales"],["elemento"=>"Ge", "clase"=>"metaloides"],["elemento"=>"As", "clase"=>"metaloides"],["elemento"=>"Se", "clase"=>"otros-no-metales"],
+  ["elemento"=>"Br", "clase"=>"halogenos"],["elemento"=>"Kr", "clase"=>"gases-nobles"]);
+
+  //F fluor por el momento
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla Periódica</title>
+    <link rel="stylesheet" href="folders\css\librarys\bootstrap\bootstrap.min.css">
+    <link rel="stylesheet" href="folders\css\formato.css">
+</head>
+<body>
+
+  <?php 
+ 
+    echo "Hola";
+
+
+  ?>
+    
+</body>
+</html>
