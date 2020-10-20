30c0d8adfd777dc9792cf5672c0244058fbabe2e
BrahiamCastillo <mymoviehb@gmail.com>
2020-10-20 11:49:40 -0400

    Estructura mejorada

--- Tabla.php
+++ Tabla.php
@@ -1,28 +1,47 @@
 <?php 
   ///Declaración de array con los elementos de la tabla periódica, en conjunto a las clases que poseen el color...
   ///que los distingue, en conjunto a esto, se declararán los contadores que se usarán para generar la tabla.
+
   $bucle=0;
   $fila=0;
 
   $componentes=array(["elemento"=>"H", "clase"=>"otros-no-metales"],
-  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
-  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
-  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
-  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
+
+  ///Estructura de la tabla, no se deben tocar las líneas debajo de este comentario
+  ///------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  ["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],
+  ["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],
+  ["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],
+  ["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],
+
+  ///Aquí termina la primera estructura, no se deben tocar las líneas antes de este comentario
+
   ["elemento"=>"He", "clase"=>"gases-nobles"],
   ["elemento"=>"Li", "clase"=>"alcalinos"],["elemento"=>"Be", "clase"=>"alcalinoterreos"],
 
-  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
-  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
-  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
+  ///Estructura de la tabla, no se deben tocar las líneas debajo de este comentario
+  ///------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  ["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],
+  ["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],
+  ["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],
+
+  ///Aquí termina la primera estructura, no se deben tocar las líneas antes de este comentario
 
   ["elemento"=>"B", "clase"=>"metaloides"],
   ["elemento"=>"C", "clase"=>"otros-no-metales"],["elemento"=>"N", "clase"=>"otros-no-metales"],["elemento"=>"O", "clase"=>"otros-no-metales"],
   ["elemento"=>"F", "clase"=>"halogenos"],["elemento"=>"Ne", "clase"=>"gases-nobles"],["elemento"=>"Na", "clase"=>"alcalinos"],
   ["elemento"=>"Mg", "clase"=>"alcalinoterreos"],
-  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
-  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
-  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
+
+   ///Estructura de la tabla, no se deben tocar las líneas debajo de este comentario
+   ///------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  ["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],
+  ["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],
+  ["elemento"=>"  ", "clase"=>"celdas-vacias"],["elemento"=>"  ", "clase"=>"celdas-vacias"],
+
+  ///Aquí termina la primera estructura, no se deben tocar las líneas antes de este comentario
   
   ["elemento"=>"Al", "clase"=>"otros-metales"],["elemento"=>"Si", "clase"=>"metaloides"],["elemento"=>"P", "clase"=>"otros-no-metales"],["elemento"=>"S", "clase"=>"otros-no-metales"],
   ["elemento"=>"Cl", "clase"=>"halogenos"],["elemento"=>"Ar", "clase"=>"gases-nobles"],["elemento"=>"K", "clase"=>"alcalinos"],["elemento"=>"Ca", "clase"=>"alcalinoterreos"],
@@ -54,23 +73,26 @@
  <div class="row">
    <div class="col-md-2"></div>
    <div class="col-md-8">
- <table class="celdas" id="tabla0">
-  <tr class="celdas" id="tr0">
+ <table id="tabla0">
+  <tr id="tr0">
   <?php 
   
   foreach ($componentes as $componente) {
     if($componente["elemento"]=="") {
       echo "<td WIDTH=50 HEIGHT=50 class='vacio celdas' id='filat{$fila}'>".$componente["elemento"]."</td>";
       $bucle++;
+    } else if($componente["clase"]=="celdas-vacias") {
+      echo "<td WIDTH=50 HEIGHT=50 class='".$componente["clase"]."' id='filat{$fila}'>".$componente["elemento"]."</td>";
+      $bucle++;
+
     } else {
       echo "<td WIDTH=50 HEIGHT=50 class='".$componente["clase"]." celdas' id='filat{$fila}'>".$componente["elemento"]."</td>";
       $bucle++;
 
-      if($bucle==18) {
-        echo "<tr></tr>";
-        $bucle=0;
-      }
-
+    }
+    if($bucle==18) {
+      echo "<tr></tr>";
+      $bucle=0;
     }
    
   }
