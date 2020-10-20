4bb20a4544fac1acba3501e634bb1f1b5e08bc3f
BrahiamCastillo <mymoviehb@gmail.com>
2020-10-19 23:14:43 -0400

    Casi terminado

--- Tabla.php
+++ Tabla.php
@@ -1,10 +1,15 @@
 <?php 
+  ///Declaración de array con los elementos de la tabla periódica, en conjunto a las clases que poseen el color...
+  ///que los distingue, en conjunto a esto, se declararán los contadores que se usarán para generar la tabla.
+  $bucle=0;
+  $fila=0;
+
   $componentes=array(["elemento"=>"H", "clase"=>"otros-no-metales"],
   ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
   ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
   ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
   ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
-  ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"He", "clase"=>"gases-nobles"],
+  ["elemento"=>"He", "clase"=>"gases-nobles"],
   ["elemento"=>"Li", "clase"=>"alcalinos"],["elemento"=>"Be", "clase"=>"alcalinoterreos"],
 
   ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
@@ -13,7 +18,7 @@
 
   ["elemento"=>"B", "clase"=>"metaloides"],
   ["elemento"=>"C", "clase"=>"otros-no-metales"],["elemento"=>"N", "clase"=>"otros-no-metales"],["elemento"=>"O", "clase"=>"otros-no-metales"],
-  ["elemento"=>"F", "clase"=>"halogeno"],["elemento"=>"Ne", "clase"=>"gases-nobles"],["elemento"=>"Na", "clase"=>"alcalinos"],
+  ["elemento"=>"F", "clase"=>"halogenos"],["elemento"=>"Ne", "clase"=>"gases-nobles"],["elemento"=>"Na", "clase"=>"alcalinos"],
   ["elemento"=>"Mg", "clase"=>"alcalinoterreos"],
   ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
   ["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],["elemento"=>"", "clase"=>"vacio"],
@@ -24,12 +29,13 @@
   ["elemento"=>"Sc", "clase"=>"metales"],["elemento"=>"Ti", "clase"=>"metales"],["elemento"=>"V", "clase"=>"metales"],["elemento"=>"Cr", "clase"=>"metales"],["elemento"=>"Mn", "clase"=>"metales"],
   ["elemento"=>"Fe", "clase"=>"metales"],["elemento"=>"Co", "clase"=>"metales"],["elemento"=>"Ni", "clase"=>"metales"],["elemento"=>"Cu", "clase"=>"metales"],["elemento"=>"Zn", "clase"=>"metales"],
   ["elemento"=>"Ga", "clase"=>"otros-metales"],["elemento"=>"Ge", "clase"=>"metaloides"],["elemento"=>"As", "clase"=>"metaloides"],["elemento"=>"Se", "clase"=>"otros-no-metales"],
-  ["elemento"=>"Br", "clase"=>"halogenos"],["elemento"=>"Kr", "clase"=>"gases-nobles"]);
+  ["elemento"=>"Br", "clase"=>"halogenos"],["elemento"=>"Kr", "clase"=>"gases-nobles"],["elemento"=>"Rb", "clase"=>"alcalinos"]);
 
-  //F fluor por el momento
+  //Rb rubio por el momento
 
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -40,13 +46,41 @@
     <link rel="stylesheet" href="folders\css\formato.css">
 </head>
 <body>
-
+  <div class="row">
+    <div class="col-md-5"></div>
+    <div class="col-md-5"><h2>Tabla periódica</h2></div>
+    <div class="col-md-2"></div>
+  </div>
+ <div class="row">
+   <div class="col-md-2"></div>
+   <div class="col-md-8">
+ <table class="celdas" id="tabla0">
+  <tr class="celdas" id="hr0">
   <?php 
- 
-    echo "Hola";
+  
+  foreach ($componentes as $componente) {
+    if($componente["elemento"]=="") {
+      echo "<td WIDTH=50 HEIGHT=50 class='vacio celdas' id='filat{$fila}'>".$componente["elemento"]."</td>";
+      $bucle++;
+    } else {
+      echo "<td WIDTH=50 HEIGHT=50 class='".$componente["clase"]." celdas' id='filat{$fila}'>".$componente["elemento"]."</td>";
+      $bucle++;
 
+      if($bucle==18) {
+        echo "<tr></tr>";
+        $bucle=0;
+      }
 
-  ?>
+    }
+   
+  }
+
+?>
+  </tr>
+  </table>
+  </div>
+  <div class="col-md-2"></div>
+ </div>
     
 </body>
 </html>
